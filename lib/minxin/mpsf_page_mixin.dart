4fae14938ef321136349803b51bca57ea7d3c3a1
朱玉辉(EX-ZHUYUHUI002) <EX-ZHUYUHUI002@pingan.com.cn>
2021-02-04 11:33:40 +0800

    update

--- lib/minxin/mpsf_page_mixin.dart
+++ lib/minxin/mpsf_page_mixin.dart
@@ -128,18 +128,4 @@ mixin MpsfPageMixin<T extends StatefulWidget> on State<T> {
   void finishDartPageOrApp() {
     SystemNavigator.pop();
   }
-
-
-  ///[空置页面]
-  int blankStatus = 0;
-  String blankIconPath;
-  String blankTitle;
-  String blankDescription;
-}
-
-class MpsfBlankStatus {
-  static const int ready = 0; //就绪
-  static const int loading = 1; //请求中
-  static const int noData = 2; //空数据
-  static const int error = 3; //请求错误
 }
